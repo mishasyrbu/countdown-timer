87f2ae4e7eeb0fa875910f02f540b3d20ccc4ea2
Misha Syrbu <mihaisyrbu91@gmail.com>
2020-05-02 08:17:55 +0300

    Updates

---
+++ precache-manifest.f5165e9cbcfc5fcfacad218928129dd3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ba9790eaa405eb446cec3b4339653e1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fcbfe389faf50429f1e6",
+    "url": "/static/css/main.511aae80.chunk.css"
+  },
+  {
+    "revision": "b50e35044c933113dc44",
+    "url": "/static/js/2.466aa8b9.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.466aa8b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fcbfe389faf50429f1e6",
+    "url": "/static/js/main.7240f36e.chunk.js"
+  },
+  {
+    "revision": "6ee2ba46a34cd00b0438",
+    "url": "/static/js/runtime-main.f2eb7a5a.js"
+  },
+  {
+    "revision": "4654aff7af11eb2a5987a2fa627179c0",
+    "url": "/static/media/notification.4654aff7.ogg"
+  },
+  {
+    "revision": "90a6d5af1ca2c7dc0bd195127594dc56",
+    "url": "/static/media/notification.90a6d5af.mp3"
+  }
+]);
